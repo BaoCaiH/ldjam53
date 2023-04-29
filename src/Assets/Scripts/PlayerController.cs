9a4ca161e33401882bab1ced596a677373fc39b6
Long Vu <longvh96@gmail.com>
2023-04-29 16:01:47 +0300

    Flipping player based on direction

--- src/Assets/Scripts/PlayerController.cs
+++ src/Assets/Scripts/PlayerController.cs
@@ -30,13 +30,28 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        UpdateMovement();
+        UpdateFacingDirection();
+    }
+
+    private void UpdateMovement()
+    {
         rgbody.velocity = new Vector2(moveInput.x * walkSpeed, rgbody.velocity.y);
+    }
+
+    private void UpdateFacingDirection()
+    {
+        if ((transform.localScale.x * moveInput.x) <= -1) {
+            // Player has changed direction so here we'll flip the scale.
+            transform.localScale *= new Vector2(-1, 1);
+        }
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
     }
+
 
     public void OnAttack(InputAction.CallbackContext context)
     {
