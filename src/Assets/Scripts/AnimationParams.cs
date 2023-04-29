018a80c6eaa0b9bb71a3948ecd017143952219a6
Long Vu <longvh96@gmail.com>
2023-04-29 23:34:15 +0300

    Implement simple state machine

---
+++ src/Assets/Scripts/AnimationParams.cs
@@ -0,0 +1,5 @@
+internal class AnimationParams
+{
+    internal static string MOVE_FLAG = "isMoving";
+    internal static string ATTACK_TRIGGER = "attack";
+}
--- src/Assets/Scripts/AnimationStrings.cs
+++
@@ -1,7 +0,0 @@
-internal class AnimationStrings
-{
-    internal static string MOVE_FLAG = "isMoving";
-    internal static string attackTrigger = "attack";
-    internal static string attackWindUpTrigger = "attackWindUp";
-    internal static string attackChargeTrigger = "attackCharge";
-}
