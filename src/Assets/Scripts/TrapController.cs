b6cac1d203cb4549bed5a84987d59b34e0bdc38c
BaoCaiH <caihongbao280996@gmail.com>
2023-04-30 21:49:11 +0200

    add level and trap

---
+++ src/Assets/Scripts/TrapController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject box = collision.gameObject;
+        BoxController controller = box.GetComponent<BoxController>();
+        if (controller)
+        {
+            controller.Damage();
+            if (controller.IsBroken()) { Destroy(box); }
+        }
+    }
+}
