5434155780fe5f506cec50a642d2ccd4e6c24eee
BaoCaiH <caihongbao280996@gmail.com>
2023-04-30 20:13:01 +0200

    add level 1

--- src/Assets/Scripts/TruckController.cs
+++ src/Assets/Scripts/TruckController.cs
@@ -64,7 +64,14 @@ public class TruckController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log(collision);
+        if (isTransitionIn) { return; }
+        GameObject box = collision.gameObject;
+        Destroy(box);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isTransitionIn) { return; }
         GameObject box = collision.gameObject;
         Destroy(box);
     }
