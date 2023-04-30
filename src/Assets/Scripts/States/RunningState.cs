232569a10b615e191936f40d060470d920f2b513
Long Vu <longvh96@gmail.com>
2023-04-30 17:29:31 +0300

    WIP

---
+++ src/Assets/Scripts/States/RunningState.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+class RunningState: MovingState
+{
+    public override void OnEnter(PlayerController player)
+    {
+        Debug.Log("Enter [Running State]!");
+        base.OnEnter(player);
+        player.animator.SetBool(AnimationParams.RUN_FLAG, true);
+    }
+
+    // public PlayerState OnMove(InputAction.CallbackContext context, PlayerController player) {
+    //     return null;
+    // }
+    // public PlayerState OnAttack(InputAction.CallbackContext context, PlayerController player)
+    // {
+    //     return null;
+    // }
+    // public PlayerState OnJump(InputAction.CallbackContext context, PlayerController player)
+    // {
+    //     return null;
+    // }
+
+    public override PlayerState OnRun(InputAction.CallbackContext context, PlayerController player)
+    {
+        Debug.Log($"[Running State] OnRun: started->{context.started}, performed->{context.performed}, canceled->{context.canceled}");
+        if (context.canceled)
+        {
+            // bool isMoving = player.input.actions["Move"].inProgress;
+            // return isMoving ? new MovingState() : new IdlingState();
+            return new MovingState();
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    // public new PlayerState OnUpdate(PlayerController player)
+    // {
+    //     return null;
+    // }
+
+    public override void OnExit(PlayerController player)
+    {
+        player.animator.SetBool(AnimationParams.RUN_FLAG, false);
+    }
+
+    protected override float GetMoveSpeed(PlayerController player)
+    {
+        return 8f;
+    }
+}
