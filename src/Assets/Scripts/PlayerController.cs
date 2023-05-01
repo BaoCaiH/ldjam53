0b8877bc17cd46c69c1029d01cee3fc8c1e9faf1
Long Vu <longvh96@gmail.com>
2023-05-01 15:03:40 +0300

    Fix backward glove

--- src/Assets/Scripts/PlayerController.cs
+++ src/Assets/Scripts/PlayerController.cs
@@ -198,5 +198,6 @@ public class PlayerController : MonoBehaviour
                 currentWeapon = new BackwardGlove();
                 break;
         }
+        currentWeapon.SetFacing(facing);
     }
 }
