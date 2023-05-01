4a00508c0507b4947f6cf97ee8866d86e50c3b9f
Long Vu <longvh96@gmail.com>
2023-05-01 20:10:02 +0300

    Add wind burst effect and update heavy glove accordingly

--- src/Assets/Scripts/Weapons/HeavyGlove.cs
+++ src/Assets/Scripts/Weapons/HeavyGlove.cs
@@ -10,12 +10,18 @@ class HeavyGlove : Weapon
     {
         Debug.Log($"[HeavyGlove] OnAttach");
         player.rgbody.gravityScale = 15;
+        player.rgbody.mass = 3;
+        player.walkSpeed = PlayerController.DEFAULT_WALK_SPEED;
+        player.runSpeed = PlayerController.DEFAULT_RUN_SPEED - 2;
     }
 
     public void OnDetach(PlayerController player)
     {
         Debug.Log($"[HeaveGlove] OnDetach");
         player.rgbody.gravityScale = 7;
+        player.rgbody.mass = 1;
+        player.walkSpeed = PlayerController.DEFAULT_WALK_SPEED;
+        player.runSpeed = PlayerController.DEFAULT_RUN_SPEED;
     }
 
     public void Apply(GameObject gameObject)
